342a25eebb4311dd952958cdbc6d0b139c8138c2
marsRocha <marciorocha.dev@gmail.com>
2024-09-13 22:51:08 +0100

    Create API

---
+++ event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.marciorocha.event_driven_activity_tracking.controller;
+
+import com.marciorocha.event_driven_activity_tracking.kafka.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class KafkaController {
+
+    @Autowired
+    private MessageProducer messageProducer;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("my-topic", message);
+        return "Message sent: " + message;
+    }
+
+}
