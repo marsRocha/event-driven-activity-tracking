865b3c29bef606bbbd42c7fc8473915eea1acf6e
marsRocha <marciorocha.dev@gmail.com>
2024-09-13 22:15:31 +0100

    Create base project

---
+++ event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/EventDrivenActivityTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.marciorocha.event_driven_activity_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenActivityTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenActivityTrackingApplication.class, args);
+	}
+
+}
