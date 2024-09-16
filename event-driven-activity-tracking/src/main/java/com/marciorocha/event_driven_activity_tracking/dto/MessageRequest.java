7ebf22498a3aef7f9ba3d40422cc46353411829b
marsRocha <marciorocha.dev@gmail.com>
2024-09-16 19:10:55 +0100

    Add lombok and dependency injection

--- event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/dto/MessageRequest.java
+++ event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/dto/MessageRequest.java
@@ -1,63 +1,19 @@
 package com.marciorocha.event_driven_activity_tracking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
-/*
-public record MessageRequest (
-        Integer userId,
-        String activityType,
-        String pageUrl,
-        LocalDateTime timestamp
-) {
-}
-*/
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageRequest {
-    private Integer userId;
+    private String userId;
     private String activityType;
     private String pageUrl;
     private LocalDateTime timestamp;
-
-    public MessageRequest() {}
-
-    public MessageRequest(Integer userId, String activityType, String pageUrl, LocalDateTime timestamp){
-        this.userId = userId;
-        this.activityType = activityType;
-        this.pageUrl = pageUrl;
-        this.timestamp = timestamp;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getActivityType() {
-        return activityType;
-    }
-
-    public void setActivityType(String activityType) {
-        this.activityType = activityType;
-    }
-
-    public String getPageUrl() {
-        return pageUrl;
-    }
-
-    public void setPageUrl(String pageUrl) {
-        this.pageUrl = pageUrl;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 
     @Override
     public String toString() {
--- event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/kafka/MessageConsumer.java
+++ event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/kafka/MessageConsumer.java
@@ -1,15 +1,24 @@
 package com.marciorocha.event_driven_activity_tracking.kafka;
 
+import com.marciorocha.event_driven_activity_tracking.UserActivityService;
 import com.marciorocha.event_driven_activity_tracking.dto.MessageRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class MessageConsumer {
+
+    @Autowired
+    private final UserActivityService userActivityService;
 
     @KafkaListener(topics = "user-activity", groupId = "my-group-id", containerFactory = "kafkaListenerContainerFactory")
     public void listen(MessageRequest message) {
         // Process and store the activity in MongoDB
+        userActivityService.RegisterUserActivity(message);
+
         System.out.println("Received message: " + message);
     }
 }
