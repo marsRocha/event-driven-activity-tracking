c73b12ce75a15c0a475f0ae336362a44cc5be6f1
marsRocha <marciorocha.dev@gmail.com>
2024-09-16 19:34:12 +0100

    Add get endpoints

--- event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/UserActivityService.java
+++ event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/UserActivityService.java
@@ -6,12 +6,22 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class UserActivityService {
 
     @Autowired
     private final UserActivityRepository userActivityRepository;
+
+    public List<UserActivity> GetAllUserActivities() {
+        return userActivityRepository.findAll();
+    }
+
+    public List<UserActivity> GetUserActivitiesByUserId(String userId) {
+        return userActivityRepository.findByUserId(userId);
+    }
 
     public void RegisterUserActivity(MessageRequest messageRequest) {
 
--- event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/controller/KafkaController.java
+++ event-driven-activity-tracking/src/main/java/com/marciorocha/event_driven_activity_tracking/controller/KafkaController.java
@@ -1,21 +1,40 @@
 package com.marciorocha.event_driven_activity_tracking.controller;
 
+import com.marciorocha.event_driven_activity_tracking.UserActivity;
+import com.marciorocha.event_driven_activity_tracking.UserActivityService;
 import com.marciorocha.event_driven_activity_tracking.dto.MessageRequest;
 import com.marciorocha.event_driven_activity_tracking.kafka.MessageProducer;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/activity")
+@AllArgsConstructor
 public class KafkaController {
 
     @Autowired
-    private MessageProducer messageProducer;
+    private final UserActivityService userActivityService;
+    @Autowired
+    private final MessageProducer messageProducer;
+
+    @GetMapping
+    public List<UserActivity> getAllActivities() {
+        return userActivityService.GetAllUserActivities();
+    }
 
     @PostMapping
     public String sendMessage(@RequestBody MessageRequest messageRequest) {
         messageProducer.sendMessage("user-activity", messageRequest);
         return "Message sent" + messageRequest;
     }
+
+    @GetMapping("/{userId}")
+    public List<UserActivity> getUserActivities(@PathVariable String userId) {
+        return userActivityService.GetUserActivitiesByUserId(userId);
+    }
+
 
 }
