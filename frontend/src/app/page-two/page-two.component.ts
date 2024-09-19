15173fb84e332f2f7ecb193beaa8df9149317c92
marsRocha <marciorocha.dev@gmail.com>
2024-09-19 21:10:28 +0100

    Create page two

---
+++ frontend/src/app/page-two/page-two.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-two',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-two.component.html',
+  styleUrl: './page-two.component.css'
+})
+export class PageTwoComponent {
+
+}
