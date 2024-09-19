706cd0f40c196cf2124bba8732a7f6d214375fd7
marsRocha <marciorocha.dev@gmail.com>
2024-09-19 21:10:03 +0100

    Create page one

---
+++ frontend/src/app/page-one/page-one.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-one',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-one.component.html',
+  styleUrl: './page-one.component.css'
+})
+export class PageOneComponent {
+
+}
