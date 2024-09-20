240ff395c5463595b793e58842d4a669ab6ea80c
marsRocha <marciorocha.dev@gmail.com>
2024-09-20 23:54:38 +0100

    Merge all into component one on frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PageOneComponent } from './page-one/page-one.component';
-import { PageTwoComponent } from './page-two/page-two.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, PageOneComponent, PageTwoComponent, RouterOutlet],
+  imports: [NavbarComponent, PageOneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'EDUATS';
 }
--- frontend/src/app/page-one/page-one.component.ts
+++ frontend/src/app/page-one/page-one.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-one',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './page-one.component.html',
-  styleUrl: './page-one.component.css'
+  styleUrls: ['./page-one.component.css']
 })
-export class PageOneComponent {
+export class PageOneComponent implements OnInit {
 
+  activities: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  fetchActivities() {
+    this.http.get('http://localhost:8080/api/v1/activity').subscribe(
+      (response: any) => {
+        console.log(response);
+        this.activities = response;
+      },
+      (error) => {
+        console.error("Error fetching activities:", error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    var event = {activityType: 'page_visit', pageUrl: '/home', timestamp: new Date().toISOString()}
+    this.http.post("http://localhost:8080/api/v1/activity", event)
+    .subscribe(
+      (response) => console.log("Event sent successfully", response),
+      (error) => console.error("Error sending event", error)
+    );
+
+    this.fetchActivities()
+  }
+
+  onButtonClick(): void {
+    var event = {activityType: 'button_click', pageUrl: '/home', timestamp: new Date().toISOString()}
+    this.http.post("http://localhost:8080/api/v1/activity", event)
+    .subscribe(
+      (response) => console.log("Event sent successfully", response),
+      (error) => console.error("Error sending event", error)
+    );
+
+    this.fetchActivities()
+  }
+
+  onClearClick(): void {
+    this.http.delete("http://localhost:8080/api/v1/activity").subscribe()
+
+    this.fetchActivities()
+  }
 }
